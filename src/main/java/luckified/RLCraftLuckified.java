a7481b77c426c216380df2986a22d48606b514e2
Nischhelm <nischhelm@gmail.com>
2024-11-03 21:28:34 +0100

    1.0.5 serverside only

--- src/main/java/luckified/RLCraftLuckified.java
+++ src/main/java/luckified/RLCraftLuckified.java
@@ -9,10 +9,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import luckified.handlers.ModRegistry;
 
-@Mod(modid = RLCraftLuckified.MODID, version = RLCraftLuckified.VERSION, name = RLCraftLuckified.NAME, dependencies = "required-after:fermiumbooter")
+@Mod(modid = RLCraftLuckified.MODID, version = RLCraftLuckified.VERSION, name = RLCraftLuckified.NAME, dependencies = "required-after:fermiumbooter", acceptableRemoteVersions = "*")
 public class RLCraftLuckified {
     public static final String MODID = "luckified";
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.0.5";
     public static final String NAME = "RLCraft Luckified";
     public static final Logger LOGGER = LogManager.getLogger();
 
