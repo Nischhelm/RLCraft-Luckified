fa426abb320cc85d2154bbcc42d6205e285bd45a
Nischhelm <nischhelm@gmail.com>
2025-10-04 12:05:22 +0200

    v1.1.2
    - updated gradle
    - added SME enchanting focus attribute modfier for luck potion effect
    - added note for existing incompat with noexpensive so it doesnt crash with it

--- src/main/java/luckified/Luckified.java
+++ src/main/java/luckified/Luckified.java
@@ -21,7 +21,7 @@ import java.util.stream.Collectors;
 )
 public class Luckified {
     public static final String MODID = "luckified";
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.1.2";
     public static final String NAME = "RLCraft Luckified";
     public static final Logger LOGGER = LogManager.getLogger();
 
